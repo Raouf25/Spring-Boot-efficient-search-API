1226381ddc36d8ef22cb79b99d4e1c51c484945a
Raouf MAKHLOUF <makhlouf.raouf@gmail.com>
2023-02-08 00:12:13 +0100

    fix: upgrade maven dependencies

--- src/main/java/com/mak/springbootefficientsearchapi/controller/CarController.java
+++ src/main/java/com/mak/springbootefficientsearchapi/controller/CarController.java
@@ -19,20 +19,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Collection;
@@ -114,12 +104,12 @@ public class CarController {
     @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<List<Car>> uploadFile(@RequestParam(value = "files") List<MultipartFile> files) {
         List<Car> cars = files.stream()
-                              .parallel()
-                              .map(unchecked(carService::uploadFile))
-                              .flatMap(Collection::stream)
-                              .collect(Collectors.toList());
+                .parallel()
+                .map(unchecked(carService::uploadFile))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
         return ResponseEntity.status(HttpStatus.CREATED)
-                             .body(cars);
+                .body(cars);
     }
 
     @GetMapping(value = "/extract", produces = {MediaType.APPLICATION_OCTET_STREAM_VALUE})
@@ -139,9 +129,9 @@ public class CarController {
 
         LocalDateTime now = LocalDateTime.now();
         return ResponseEntity.ok()
-                             .contentType(MediaType.parseMediaType("application/octet-stream"))
-                             .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"Search_Extraction_" + now + ".csv\"")
-                             .body(resource);
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"Search_Extraction_" + now + ".csv\"")
+                .body(resource);
     }
 
 
