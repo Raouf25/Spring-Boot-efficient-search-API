5152c7d5d124569a336bd2458d9b654af465e477
Raouf MAKHLOUF <makhlouf.raouf@gmail.com>
2023-02-11 00:22:02 +0100

    fix: downgrade version

--- src/main/java/com/mak/springbootefficientsearchapi/entity/Car.java
+++ src/main/java/com/mak/springbootefficientsearchapi/entity/Car.java
@@ -1,11 +1,12 @@
 package com.mak.springbootefficientsearchapi.entity;
 
-import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Getter
