66507e5cd1fb25767b528f36660831688a1c328c
Raouf MAKHLOUF <makhlouf.raouf@gmail.com>
2023-05-08 20:10:23 +0200

    feat add cache

--- src/main/java/com/mak/springbootefficientsearchapi/SpringBootEfficientSearchApiApplication.java
+++ src/main/java/com/mak/springbootefficientsearchapi/SpringBootEfficientSearchApiApplication.java
@@ -4,8 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
+@EnableCaching
 @SpringBootApplication
 public class SpringBootEfficientSearchApiApplication {
 
