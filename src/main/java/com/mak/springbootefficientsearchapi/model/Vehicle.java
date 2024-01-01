8c176903b9bff8afe9eee88425cb7f8f7057338d
Raouf MAKHLOUF <makhlouf.raouf@gmail.com>
2024-01-01 13:34:27 +0100

    feat: use java record for model

--- src/main/java/com/mak/springbootefficientsearchapi/model/Vehicle.java
+++ src/main/java/com/mak/springbootefficientsearchapi/model/Vehicle.java
@@ -1,21 +1,6 @@
 package com.mak.springbootefficientsearchapi.model;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDate;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Vehicle {
-    private String manufacturer;
-    private String model;
-    private String type;
-    private String country;
-    private LocalDate createDate;
+public record Vehicle(String manufacturer, String model, String type, String country, LocalDate createDate) {
 }
